41341abc6a7ef3d10489290d0433543fc8043f61
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-02 15:33:20 +0200

    add UsersList component

---
+++ src/components/Users/UsersList/UsersList.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import UserData from "../../../types/user";
+import { Card } from "../../UI/Card";
+
+const UsersCard = styled(Card)`
+    margin: 2rem auto;
+    width: 90%;
+    max-width: 40rem;
+
+    & ul {
+        list-style: none;
+        padding: 1rem;
+    }
+
+    & li {
+        border: 1px solid #ccc;
+        margin: 0.5rem 0;
+        padding: 0.5rem;
+    }
+`
+
+interface UserListProps {
+    users: UserData[]
+}
+
+function UsersList({ users }: UserListProps) {
+    return (
+        <UsersCard>
+            <ul>
+                {users.map(u=> (
+                    <li>
+                        {u.name} ({u.age} years old)
+                    </li>
+                ))}
+            </ul>
+        </UsersCard>
+    );
+}
+
+export default UsersList;
