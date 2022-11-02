a35a58bb934613209cc66872eda25eb06339e144
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-02 15:33:51 +0200

    update AddUser component

--- src/components/Users/AddUser/AddUser.tsx
+++ src/components/Users/AddUser/AddUser.tsx
@@ -1,19 +1,42 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { Button } from "../../UI/Button";
 import Input from "../../UI/Input/Input";
 import { UserCard } from "./styles";
 
 function AddUser() {
+    const [username, setUsername] = useState('')
+    const [age, setAge] = useState('')
 
     function addUserHandler(e:FormEvent<HTMLFormElement>) {
         e.preventDefault()
+
+        // validation
+        if (username.trim() === '' || age.trim() === '') {
+            return
+        }
+        if (+age < 0) {
+            return
+        }
+
+        setUsername('')
+        setAge('')
     }
 
     return (
         <UserCard>
             <form onSubmit={addUserHandler}>
-                <Input id="username" label="Username"/>
-                <Input id="age" label="Age (Years)"/>
+                <Input 
+                    id="username" 
+                    label="Username" 
+                    onChange={e=> setUsername(e.target.value)} 
+                    value={username}
+                />
+                <Input 
+                    id="age" 
+                    label="Age (Years)" 
+                    onChange={e=>setAge(e.target.value)}
+                    value={age}
+                />
                 <Button type="submit">Add User</Button>
             </form>
         </UserCard>
