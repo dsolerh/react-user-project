fca9bc5a7bf7388f5e5b3bfa9beb05d04400a68c
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-04 08:44:47 +0200

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import AddUser from './components/Users/AddUser/AddUser';
+import UsersList from './components/Users/UsersList/UsersList';
+import UserData from './types/user';
 
 function App() {
+  const [users, setUsers] = useState<UserData[]>([])
   return (
     <div>
-      <AddUser/>
+      <AddUser onAddNewUser={u=>setUsers(prev=>[...prev, u])}/>
+      <UsersList users={users}/>
     </div>
   );
 }
